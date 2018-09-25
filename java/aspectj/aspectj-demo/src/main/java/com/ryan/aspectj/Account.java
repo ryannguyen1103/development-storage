fb8d55d8a35c6a0297589bf6b96d0f1ef08d2b70
Ryan <duy.nguyen@code88.asia>
2018-09-25 16:41:05 +0700

    change project name

---
+++ java/aspectj/aspectj-demo/src/main/java/com/ryan/aspectj/Account.java
@@ -0,0 +1,18 @@
+package com.ryan.aspectj;
+
+/**
+ *
+ * @author ryan.nguyen
+ */
+public class Account {
+
+    int balance = 20;
+
+    public boolean withdraw(int amount) {
+        if (balance < amount) {
+            return false;
+        }
+        balance = balance - amount;
+        return true;
+    }
+}
